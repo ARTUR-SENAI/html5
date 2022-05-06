07dca0ae5fa7f8d17b5541d5cecc62b145a095aa
ARTUR-SENAI <0000880096@senaimgaluno.com.br>
2022-05-06 19:06:46 -0300

    nagaragem-----

---
+++ javascript/cookie.js
@@ -0,0 +1,33 @@
+function checkCookie (){
+    var user=getCookie("user");
+    var msg= document.getElementById("msg");
+    if (user!=null && user!=""){
+        msg.innerHTML="Bem-vindo de volta"
+    }else {
+        user =prompt ("Digite seu nome:", "");
+        if(user!=null && user!=""){
+            setCookie ("user", user,365);
+            msg.innerHTML="Bem-vindo"+user;
+        }
+    }
+}
+function getCookie(c_name){
+    var c_value=document.cookie;
+    var c_start= c_value.indexOf(""+c_name+"=");
+    if (c_start==-1){
+        c_start=c_value.indexOf(c_name+"=");
+
+    }
+    if(c_start==-1){
+        c_value=null;
+
+    }else{
+        c_start=c_value.indexOf("=",c_start)+1;
+        var c_end=c_value.indexOf(";", c_start);
+        if(c_end==-1){
+            c_end=c_value.length;
+        }
+        c_value= unescape(c_value.substring(c_start,c_end));
+    }
+    return c_value
+}
